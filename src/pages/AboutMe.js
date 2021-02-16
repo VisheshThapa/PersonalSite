7e0370846fd7cae20aaaebdc53e1d53ec0d65e4f
Vishesh Thapa <visheshjthapa@gmail.com>
2021-02-16 22:39:19 +0545

    updates

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -16,7 +16,11 @@ British Columbia's Okanagan Campus. From a young age I have always been
 </p>
 
 <p class = "aboutme_text_p">
-I chose Mechanical Engineering because I wanted to work in the field of robotics. This is one of the reasons that I chose mechatronics as my specializtion. Iutside of my university, I study concepts related to electrical engineering, business and computer science. Currently, I am reading "Practical Electronics for Inventors" by Paul Scherz and Simon Monk
+I chose Mechanical Engineering because I wanted to work in the field of robotics. 
+This is one of the reasons that I chose mechatronics as my specializtion. 
+Outside of my university, I study concepts related to electrical engineering, 
+business and computer science. Currently, I am reading <em>Practical Electronics for Inventors</em> 
+by Paul Scherz and Simon Monk.
 
 </p>
 
