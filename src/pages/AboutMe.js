6b9cef8d160c6bd5401732df3f6674682999dc2f
Vishesh Thapa <visheshjthapa@gmail.com>
2021-02-16 22:46:42 +0545

    projects paragraph

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -19,7 +19,7 @@ British Columbia's Okanagan Campus. From a young age I have always been
 I chose Mechanical Engineering because I wanted to work in the field of robotics. 
 This is one of the reasons that I chose mechatronics as my specializtion. 
 Outside of my university, I study concepts related to electrical engineering, 
-business and computer science. Currently, I am reading <em>Practical Electronics for Inventors</em> 
+business and computer science. Currently, I am reading <em>Practical Electronics for Inventors </em>
 by Paul Scherz and Simon Monk.
 
 </p>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,7 +17,7 @@ return(
       
       <img  src = {noKnot} alt = "noKnotImg" style={{height: "20rem", width: "auto"}}></img> 
 
-      <p style = {{marginTop:"10px"}}>This is a 3D printed wiremagement system which was created in solid works. It comes two main parts, which is the base and the screws, along with several accessocies which can be attached to the base. The screws are threaded so they easily fit into the base and can be stuck to any desk. This block allows for a center area where wires can be concentrated into. This was created in partnership with my solidworks team at UBC.</p>
+      <p style = {{marginTop:"10px"}}>This is a 3D printed wiremagement system which was created in solid works. It comes in two main parts, the base and the screws, along with several accessories which can be attached to the base. The screws are threaded so they easily fit into the base and can be stuck to any desk. This block allows for a center area where wires can be concentrated into. This was created in partnership with my solidworks team at UBC.</p>
         <hr style = {{marginTop:"10px"}}/>
     </div>
     
@@ -25,7 +25,7 @@ return(
       <h2 style={{marginBottom: "10px"}}>Random Workout Generator</h2>
       
       <img  src = {workoutimg} alt = "workoutImg" style={{height: "400px"}}></img> 
-      <p style = {{marginTop:"10px"}}>This is a random workout generator which was created in Python with the use of tkinter library. The python application works by reading a txt file which contains the excercise list and creates a workout based on the inputs the user gives. The user can pick the body part, the number of sets and number of excercies per body part. Currently, the greatest limitation of this application is that it can only access the excercises that are in the txt file. I would like to improve it further by the use of the BeautifulSoup library allowing the application to acces online resources to gain a multitudes to excercies that the application can use.</p>
+      <p style = {{marginTop:"10px"}}>This is a random workout generator which was created in Python with the use of tkinter library. The python application works by reading a txt file which contains the exercise list and creates a workout based on the inputs the user gives. The user can pick the body part, the number of sets and number of exercises per body part. Currently, the greatest limitation of this application is that it can only access the exercises that are in the txt file. I would like to improve it further by the use of the BeautifulSoup library allowing the application to acces online resources to gain a multitudes to exercise that the application can use.</p>
         <hr style = {{marginTop:"10px", marginBottom: "20px"}}/>
     </div>
 
